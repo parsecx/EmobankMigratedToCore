2ded66558e93116cef13a3f4485d5a763e66c662
parsecx <parsec.iban@gmail.com>
2023-11-01 23:36:10 +0200

    Add project files.

---
+++ EmobankMig_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmobankMig_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
